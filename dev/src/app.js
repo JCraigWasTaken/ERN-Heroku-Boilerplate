736e26ed2e221ad7af69fa35459bcd8dd5d68278
John Craig <john22.craig@gmail.com>
2019-12-03 23:59:41 -0500

    Add Frontend and Backend
    - Running in dev mode locally, and running a build locally worked as expected.
    
    - Will attempt auto build next

---
+++ dev/src/app.js
@@ -0,0 +1,37 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+
+import './app.css'
+
+class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    openLink = (link) =>{
+        const a = document.createElement('a');
+        a.setAttribute('hidden','');
+        a.setAttribute('href', link);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+
+    openLinkNewTab = (link) =>{
+        window.open(link, "_blank")
+    }
+
+    render(){
+        return(
+            <div 
+                id='appContents' 
+                className='color-background_primary color-text_primary'
+                style={{overflowY:'hidden'}}>
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<App/>,document.getElementById("app"))
